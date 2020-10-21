6c8c78fb372841fa1c75d270c1d3c044797275a1
Sai Teja <37472103+teja876@users.noreply.github.com>
2020-10-21 21:10:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,33 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: false
+    };
+  }
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  render() {
+    const handelClick = () => {
+      this.setState({ view: true });
+    };
+
+    return (
+      <div id="main">
+        {/* Do not remove this main div!! */}
+        <button id="click" onClick={handelClick}>
+          {this.view === true ? (
+            <p id="para">
+              Hello, I've learnt to use the full-stack evaluation tool. This
+              makes me so happy
+            </p>
+          ) : null}
+        </button>
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
